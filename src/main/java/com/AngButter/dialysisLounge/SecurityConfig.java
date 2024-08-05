0954e281bd10b14a53248f7120b892f32f48ff96
na-gyeongwon <heyzel77@g.eulji.ac.kr>
2024-08-05 23:23:17 +0900

    1

--- src/main/java/com/AngButter/dialysisLounge/SecurityConfig.java
+++ src/main/java/com/AngButter/dialysisLounge/SecurityConfig.java
@@ -25,27 +25,11 @@ public class SecurityConfig {
                 .headers((headers) -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
-                //로그인 설정 담당 기능
-                .formLogin((formLogin) -> formLogin
-                        .loginPage("/user/login") //로그인 폼 url
-                        .defaultSuccessUrl("/question/list")) //로그인 성공시 이동할 url
-                //로그아웃 설정 담당
-                .logout((logout) -> logout
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout")) //로그아웃 url
-                        .logoutSuccessUrl("/question/list") //로그아웃 시 이동 url
-                        .invalidateHttpSession(true)) // 사용된 사용자 세션 삭제
+
         ;
         return http.build();
     }
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+
     }
 
-    @Bean
-    //시큐리티의 인증을 처리하는 빈. UserSecurityService, PasswordEncoder 내부적 사용하여 권한, 인증 프로세스 처리
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-}
